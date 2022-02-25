0a283a3964479266ef005ac1957757ac10ec1cbf
Denis Dumont <denis.dumont09@gmail.com>
2022-02-25 14:21:14 +0100

    :rocket: add service worker

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import Favs from "./views/Favs";
 import Search from "./views/Search";
 import City from "./views/City";
 import Map from "./views/Map";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const rootElement = document.getElementById('root');
 
@@ -33,6 +34,7 @@ ReactDOM.render(
     rootElement
 );
 
+serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
